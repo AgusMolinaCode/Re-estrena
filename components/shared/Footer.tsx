134b0010af81e6b0411d9209e371534306cd6450
Agustin <am.motos@hotmail.com>
2024-03-11 22:39:12 -0300

    first commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='border-t'>
+      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flew-row'>
+        <Link href='/'>
+          <Image src='/assets/images/logo.svg' alt='Fiesta Logo' width={128} height={38} />
+        </Link>
+
+        <p>2024 Even-Dev. All rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ lib/mongodb/database/models/user.model.ts
@@ -0,0 +1,15 @@
+import { Schema, model, models } from "mongoose";
+import { unique } from "next/dist/build/utils";
+
+const UserSchema = new Schema({
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  photo: { type: String, required: true },
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
