fa07e176a0ffff662e27f4fa7bbb92fbc786a05b
Agustin <am.motos@hotmail.com>
2024-03-13 13:34:45 -0300

    Correcciones y mejoras en el código

--- lib/mongodb/database/index.ts
+++ lib/mongodb/database/index.ts
@@ -1,24 +1,20 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
+  if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URI) {
-    throw new Error("Please define the MONGODB_URI ERROR");
-  }
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URI, {
-      dbName: "eventdev",
-      bufferCommands: false,
-    });
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'eventdev',
+    bufferCommands: false,
+  })
 
   cached.conn = await cached.promise;
-};
+
+  return cached.conn;
+}
--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,21 @@
 import { authMiddleware } from "@clerk/nextjs";
-
+ 
 export default authMiddleware({
   publicRoutes: [
-    "/",
-    "/events/:id",
-    "/api/webhook/clerk",
-    "/api/webhook/stripe",
-    "/api/uploadthing",
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
   ],
   ignoredRoutes: [
-    "/api/webhook/clerk",
-    "/api/webhook/stripe",
-    "/api/uploadthing",
-  ],
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
 });
-
+ 
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+ 
