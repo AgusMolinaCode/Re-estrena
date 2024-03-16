a651a8703bd3e526054a022c3228b6429ceb8487
Agustin <am.motos@hotmail.com>
2024-03-15 23:03:10 -0300

    Actualización de dependencias en package.json***

---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,177 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+type EventFormProps = {
+  userId: string;
+  type: "Create" | "Update";
+};
+import { eventFormSchema } from "@/lib/validator";
+import { eventDefaultValues } from "@/constants";
+import Dropdown from "./Dropdown";
+import {FileUploader} from "./FileUploader";
+import { useState } from "react";
+import Image from "next/image";
+
+const EventForm = ({ userId, type }: EventFormProps) => {
+  const [files, setFiles] = useState<File[]>([]);
+  const initialValues = eventDefaultValues;
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: initialValues,
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-6"
+      >
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    placeholder="Event Title"
+                    {...field}
+                    className="input-field"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="categoryId"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Dropdown
+                    onChangeHandler={field.onChange}
+                    value={field.value}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Textarea
+                    placeholder="Description"
+                    {...field}
+                    className="textarea rounded-2xl"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <FileUploader
+                    onFieldChange={field.onChange}
+                    imageUrl={field.value}
+                    setFiles={setFiles}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-5 md:flex-row">
+        <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                    <Image
+                      src="/assets/icons/location-grey.svg"
+                      alt="location"
+                      width={24}
+                      height={24}
+                    />
+                  <Input
+                    placeholder="Event Location or Online"
+                    {...field}
+                    className="input-field"
+                  />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="flex flex-col gap-5 md:flex-row">
+        <FormField
+            control={form.control}
+            name="startDateTime"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                    <Image
+                      src="/assets/icons/calendar.svg"
+                      alt="calender"
+                      width={24}
+                      height={24}
+                    />
+                    <p className="ml-3 whitespace-nowrap text-gray-500">Start Date:</p>
+                  
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default EventForm;
---
+++ lib/validator.ts
@@ -0,0 +1,15 @@
+import * as z from "zod";
+
+export const eventFormSchema = z.object({
+    title: z.string().min(3, { message: "Title must be at least 3 characters long" }),
+    description: z.string().min(10, { message: "Description must be at least 10 characters long" }).max(1000, { message: "Description must be at most 1000 characters long" }),
+    location: z.string().min(3, { message: "Location must be at least 3 characters long" }).max(100, { message: "Location must be at most 100 characters long" }),
+    imageUrl: z.string().url({ message: "Image URL must be a valid URL" }),
+    startDateTime:z.date(),
+    endDateTime:z.date(),
+    categoryId: z.string(),
+    price: z.string(),
+    isFree: z.boolean(),
+    url: z.string().url({ message: "URL must be a valid URL" }),
+  });
+
