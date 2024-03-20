d53c94f271adaebb0489e2e7cf789efcca1de146
Agustin Molina <am.motos@hotmail.com>
2024-03-19 23:26:45 -0300

    Actualizar texto de encabezado en página principal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Plan your next party with Fiesta!</h1>
+            <h1 className="h1-bold">¡Plan your next party with Fiesta!</h1>
             <p className="p-regular-20 md:p-regular-24">
               Fiesta is a party planning app that helps you plan your next
               party. You can create events, invite friends, and keep track of
