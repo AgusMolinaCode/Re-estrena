7fa70ec15692e07d2d8bd4cbfab378fe9c850506
Agustin Molina <am.motos@hotmail.com>
2024-03-25 21:22:10 -0300

    Actualizar CheckoutButton.tsx y event.model.ts

---
+++ components/shared/Checkout.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { IEvent } from "@/lib/mongodb/database/models/event.model";
+
+import { loadStripe } from "@stripe/stripe-js";
+import { checkoutOrder } from "@/lib/actions/order.actions";
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+
+const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
+  React.useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you’re ready."
+      );
+    }
+  }, []);
+
+  const onCheckout = async () => {
+    const order = {
+        eventTitle: event.title,
+        eventId: event._id,
+        price: event.price,
+        isFree: event.isFree,
+        buyerId: userId,
+    }
+
+    await checkoutOrder(order);
+  };
+
+  return (
+    <form action={onCheckout} method="post">
+      <button type="submit">
+        {event.isFree ? "Get Ticket" : "Buy Ticket"}
+      </button>
+    </form>
+  );
+};
+
+export default Checkout;
--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -1,15 +1,32 @@
-'use client'
+"use client";
 
-import { IEvent } from '@/lib/mongodb/database/models/event.model'
-import React from 'react'
+import { IEvent } from "@/lib/mongodb/database/models/event.model";
+import { SignedIn, SignedOut, useUser } from "@clerk/nextjs";
+import React from "react";
+import Checkout from "./Checkout";
 
-const CheckoutButton = ({event}:{event: IEvent}) => {
-  
-    const hasEventFinished = new Date(event.endDateTime) < new Date();
-  
-    return (
-    <div>CheckoutButton</div>
-  )
-}
+const CheckoutButton = ({ event }: { event: IEvent }) => {
+  const { user } = useUser();
+  const userId = user?.publicMetadata.userId as string;
+  const hasEventFinished = new Date(event.endDateTime) < new Date();
 
-export default CheckoutButton
+  return (
+    <div>
+      {hasEventFinished ? (
+        <p>This event has already finished</p>
+      ) : (
+        <>
+          <SignedOut>
+            <p>You need to be signed in to book this event</p>
+          </SignedOut>
+
+          <SignedIn>
+            <Checkout event={event} userId={userId} />
+          </SignedIn>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default CheckoutButton;
